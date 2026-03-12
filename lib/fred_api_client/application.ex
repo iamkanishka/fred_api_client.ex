b0c0e5b2bac12d2c34dfbf7984dada4922599e75
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2026-03-12 15:28:49 +0530

    Added cache to application configuration

--- lib/fred_api_client/application.ex
+++ lib/fred_api_client/application.ex
@@ -2,9 +2,16 @@ defmodule FredApiClient.Application do
   @moduledoc false
   use Application
 
+  @cache_name :fred_api_cache
+
   @impl true
   def start(_type, _args) do
-    children = []
+    cache_name = Application.get_env(:fred_api_client, :cache_name, @cache_name)
+
+    children = [
+      {Cachex, name: cache_name}
+    ]
+
     opts = [strategy: :one_for_one, name: FredApiClient.Supervisor]
     Supervisor.start_link(children, opts)
   end
